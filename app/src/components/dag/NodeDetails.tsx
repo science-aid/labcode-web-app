c881a86339a4776d27d5008fa34b21b1de18f0e0
shingenmochi <kinakomochi.work@gmail.com>
2025-02-24 14:54:30 +0900

    fix displayed item in log

--- app/src/components/dag/NodeDetails.tsx
+++ app/src/components/dag/NodeDetails.tsx
@@ -80,7 +80,7 @@ export const NodeDetails: React.FC<NodeDetailsProps> = ({ node }) => {
 
         <div className="p-4 border rounded-lg shadow-sm">
           <h4 className="text-sm font-medium text-gray-500">Log</h4>
-          <p className="mt-1 text-sm text-gray-900">{node.storage_address}</p>
+          <p className="mt-1 text-sm text-gray-900">{node.log}</p>
         </div>
       </div>
     </div>
