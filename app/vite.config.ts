c44ffdb859761e3e5a1d3e0e9038e2e4b586d2eb
shingenmochi <kinakomochi.work@gmail.com>
2025-03-18 16:44:36 +0900

    move API of web-app to .env

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
+const env = loadEnv('development', process.cwd(), '');
 
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +12,7 @@ export default defineConfig({
     },
   },
   server: {
-    allowedHosts: ["labcode-web-app.com"],
+    allowedHosts: [env.ALLOWED_HOST],
     port: 5173,
     host: true,
     proxy: {
